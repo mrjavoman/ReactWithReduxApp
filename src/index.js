6e01f45fdfb96a9c0fb70b5e74b6ff3f54b79533
Xavi <mrjavoman@hotmail.com>
2017-01-07 16:57:20 -0600

    adding code to the video components from section 2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = "AIzaSyCXaqx9k8zFRAh4zU3vzQhbBzJ3cJiSodo";
 
-const API_KEY = "AIzaSyCXaqx9k8zFRAh4zU3vzQhbBzJ3cJiSodo"
+class App extends Component {
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take Component and put it in the DOM
